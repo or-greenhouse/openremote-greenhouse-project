402f350b2061dda2542e521c48d4bb769116bfd3
Dominique <10584854+dominiquekleeven@users.noreply.github.com>
2023-12-05 12:56:40 +0100

    Create HomeAssistantJsonHelper.java

---
+++ agent/src/main/java/org/openremote/agent/custom/entities/HomeAssistantJsonHelper.java
@@ -0,0 +1,33 @@
+package org.openremote.agent.custom.entities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+public class HomeAssistantJsonHelper {
+    private static final String TEST_JSON_FILE = "hass.json";
+
+    public static Optional<String> readTestJsonFile() {
+        InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(TEST_JSON_FILE);
+        assert input != null;
+        InputStreamReader streamReader = new InputStreamReader(input, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(streamReader);
+        StringBuilder output = new StringBuilder();
+        try {
+            for (String line; (line = reader.readLine()) != null; ) {
+                output.append(line);
+            }
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+        return Optional.of(output.toString());
+    }
+
+    public static String getTypeFromEntityId(String entityId) {
+        String[] parts = entityId.split("\\.");
+        return parts[0];
+    }
+}
